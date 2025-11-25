f3c9e4a760d86345e30067d8752905f111218c23
DESKTOP-ORLMGMR\webse <s2bulcot@gmail.com>
2025-11-25 22:35:06 +0900

    'working'

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,47 @@ import CalendarPage from "./pages/CalendarPage";
 
 function App() {
   return (
-    <div>
+    <div className='wrap'>
       {/* 상단 메뉴 영역 */}
-      <nav style={{ padding: "10px", borderBottom: "1px solid #ddd" }}>
-        <Link to="/" style={{ marginRight: "10px" }}>
-          Main
-        </Link>
-        <Link to="/swiper" style={{ marginRight: "10px" }}>
-          Swiper
-        </Link>
-        <Link to="/calendar">
-          Calendar
-        </Link>
-      </nav>
+      <header>
+        <h1><em className='font-logo'>js</em><strong className='font-logo-sub'>Library</strong></h1>
+        <nav>
+          <Link to="/">
+            Main
+          </Link>
+          <Link to="/swiper">
+            Slider
+          </Link>
+          <Link to="/calendar">
+            Media
+          </Link>
+          <Link to="/calendar">
+            Calendar
+          </Link>
+          <Link to="/calendar">
+            Animation
+          </Link>
+          <Link to="/calendar">
+            Gallery
+          </Link>
+          <Link to="/calendar">
+            Graphics
+          </Link>
+          <Link to="/calendar">
+            Data Visualization
+          </Link>
+          <Link to="/calendar">
+            Utilities
+          </Link>
+          <Link to="/calendar">
+            Editor
+          </Link>
+          <Link to="/calendar">
+            Search
+          </Link>
+        </nav>
+      </header>
+      
 
       {/* 라우트에 따라 페이지가 바뀌는 영역 */}
       <div style={{ padding: "20px" }}>
